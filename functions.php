d3d2ef11b5c2124bd6ece33447332b84fe5f6eb3
Eddie <eddiejudehareven@gmail.com>
2017-11-08 18:57:02 -0500

    editor-style.css

--- functions.php
+++ functions.php
@@ -95,3 +95,18 @@ function wpdocs_theme_add_editor_styles() {
 }
 add_action( 'admin_init', 'wpdocs_theme_add_editor_styles' );
 // add_editor_style( array( 'styles/modules/editor-style.css') );
+
+// Read more link //
+function herencias_excerpt_more( $link ) {
+	if ( is_admin() ) {
+		return $link;
+	}
+
+	$link = sprintf( '<p class="link-more"><a href="%1$s" class="more-link">%2$s</a></p>',
+		esc_url( get_permalink( get_the_ID() ) ),
+		/* translators: %s: Name of current post */
+		sprintf( __( 'Continue reading<span class="screen-reader-text"> "%s"</span>' ), get_the_title( get_the_ID() ) )
+	);
+	return ' &hellip; ' . $link;
+}
+add_filter( 'excerpt_more', 'herencias_excerpt_more' );
